d436f527c186d92b9d22b1b79791638a1e3ca044
Nicolas <nicovitry79@gmail.com>
2025-12-17 21:55:07 +0100

    :sparkles: Ajout du script de lancement de la pipeline

---
+++ launchPipeline.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+#SBATCH --job-name=pipeline
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nicolas.vitry@etudiant.univ-rennes.fr
+#SBATCH -o logs/pipelineLogs.out
+
+# Création de tous les répertoires utiles
+echo "Creation des répertoires utiles"
+mkdir -p genomes
+mkdir -p output
+
+# lancement de l'installation de l'environnement conda
+JOB_CONDA=$(sbatch --parsable scripts/0_utilities_scripts/conda_make_turtle.sh)
+echo "Installation de l'environnement lancé avec l'ID :"$JOB_CONDA
+
+# Téléchargement des données
+JOB_DOWNLOAD_SEQ=$(sbatch --parsable --dependency=afterok:$JOB_CONDA scripts/1_Recuperation_Data/downloadGenome.sh)
+echo "Téléchargement des données en attente avec l'ID :"$JOB_DOWNLOAD_SEQ
+
+# Script qui renome les fichier .fna en .fa pour orthofinder
+JOB_RENAME_FA=$(sbatch --parsable --dependency=afterok:$JOB_DOWNLOAD_SEQ scripts/0_utilities_scripts/renameSeq.sh)
+echo "Changement des extensions en attente avec l'ID :"$JOB_RENAME_FA
+
+# Script orthofinder
+JOB_ORTHOFINDER=$(sbatch --parsable --dependency=afterok:$JOB_RENAME_FA scripts/2_OrthoFinder/orthoFinder.sh)
+echo "Orthofinder en attente avec l'ID :"$JOB_ORTHOFINDER
+
+# Lancement script de récupération des groupes d'intérêts
+JOB_GROUP_OF_INTEREST=$(sbatch --parsable --dependency=afterok:$JOB_ORTHOFINDER scripts/3_Alignement/groupOfInterest.sh)
+echo "Recherche de groupes d'intérêts en attente avec l'ID :"$JOB_GROUP_OF_INTEREST
+
+# Lancement de l'alignement
+JOB_MAFFT=$(sbatch --parsable --dependency=afterok:$JOB_GROUP_OF_INTEREST scripts/3_Alignement/Alignement.sh)
+echo "Alignement en attente avec l'ID :"$JOB_MAFFT
+
+# Lancement du trimming des séquences
+JOB_TRIMAL=$(sbatch --parsable --dependency=afterok:$JOB_MAFFT scripts/4_Trimming/Trimal.sh)
+echo "Trimming en attente avec l'ID :"$JOB_TRIMAL
+
+# Lancement de la réconciliation
+JOB_GENERAX=$(sbatch --parsable --dependency=afterok:$JOB_TRIMAL scripts/5_Reconciliation/Reconciliation.sh)
+echo "Réconciliation en attente avec l'ID :"$JOB_GENERAX
