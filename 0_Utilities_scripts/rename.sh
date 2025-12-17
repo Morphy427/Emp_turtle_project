fe65ed1afcc7a240fba489d1d4af6e97afab2e38
Nicolas Vitry <nicovitry79@gmail.com>
2025-12-17 21:57:29 +0100

    Delete 0_Utilities_scripts directory

--- 0_Utilities_scripts/rename.sh
+++
@@ -1,6 +0,0 @@
-# Script to rename ".fna" file in ".fa" file
-
-for f in ./genomes/*.fna ; do
-	speciesID=`basename $f .fna`
-	mv $f genomes/$speciesID.fa
-done
--- 0_Utilities_scripts/subsampling.sh
+++
@@ -1,7 +0,0 @@
-# Script for subsampling the first 10 000 lines of a FASTA file
-
-for f in ./genomes/*.fa ; do
-	speciesID=`basename $f .fa`
-	head -n 10000 $f > ./genomes/ech_genomes/$speciesID\_subSample.fa
-done
-
