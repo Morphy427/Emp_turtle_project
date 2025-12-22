1684df6614748066943918c44c1c34b6a962733b
Nicolas <nicovitry79@gmail.com>
2025-12-22 23:27:26 +0100

    :recycle: Modification des chemins d'accès et du nb de coeurs

--- scripts/4_Tree_analysis/4_run_iqtree.sh
+++ scripts/4_Tree_analysis/4_run_iqtree.sh
@@ -3,22 +3,16 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nicolas.vitry@etudiant.univ-rennes.fr
 #SBATCH -o logs/iqtree.log
-#SBATCH --cpus-per-task=20
-
-
-
-#Filtre les alignements multiple pour ne garder que ceux dont la longueur est su^érieur à 650
-#Crée un arbre de gène pour chaque alignement multiple/ orthogroupe
+#SBATCH --cpus-per-task=15
 
 . /local/env/envconda.sh
 
 conda activate /home/genouest/tp_emp25_41033/tp60421/turtleProject
 
-THREADS=$SLURM_JOB_CPUS_PER_NODE
 
-inputFolder=../clean_output/alignement/trimedSeq
-outputFolder=../clean_output/iqtree/
-outputFolderAll=../clean_output/iqtree/allSpecies
+inputFolder=output/alignement/trimedSeq
+outputFolder=output/iqtree/
+outputFolderAll=output/iqtree/allTrees
 
 mkdir -p $outputFolder
 mkdir -p $outputFolderAll
@@ -50,9 +44,3 @@ for f in $outputFolder*fasta; do
 done
 
 conda deactivate
-
-# # possible options:
-# -m TESTONLY
-# -m TEST
-# -quiet
-# ... 2> /dev/null
