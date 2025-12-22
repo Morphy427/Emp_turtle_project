77103977120e21687bcbe31ee9a37d7b0d5f0164
Nicolas <nicovitry79@gmail.com>
2025-12-22 23:36:07 +0100

    :recycle: modification des chemins d'accès

--- scripts/4_Tree_analysis/4_Run_rf_distance.sh
+++ scripts/4_Tree_analysis/4_Run_rf_distance.sh
@@ -5,15 +5,19 @@
 #SBATCH -o logs/rfDistances2.log
 
 #Lance le script python permettant de calculer les scores de robinson foulds
-inputTable=../clean_output/iqtree/rf_output/results_RF.tsv
-outputTable=../clean_output/iqtree/rf_output/RF_extracted.txt
+
+rfFolder=output/iqtree/rf_output
+inputTable=$rfFolder/results_RF.tsv
+outputTable=$rfFolder/RF_extracted.txt
+
+mkdir -p $rfFolder
 
 . /local/env/envconda.sh
 
 conda activate /home/genouest/tp_emp25_41033/tp60421/turtleProject
 
-for f in ../clean_output/iqtree/allSpecies/*.treefile ; do
-    mv $f ../clean_output/iqtree/allTrees
+for f in output/iqtree/allSpecies/*.treefile ; do
+    mv $f output/iqtree/allTrees
     echo $f "moved"
     done
 
